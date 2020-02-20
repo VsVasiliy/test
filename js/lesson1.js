32de3c2d41462801443d6b3d15e28c93cbdd615e
Vasya <greenvasilisk@mail.ru>
2020-02-20 11:24:08 +0500

    homeWork1

---
+++ js/lesson1.js
@@ -0,0 +1,13 @@
+var questionUsers = +prompt("Ваш бюджет?");
+var questionUsers2 = prompt("Название вашего магазина");
+var questionUsers3 = prompt("Какой тип товара будете продавать?")
+var questionUsers4 = prompt("Какой тип товара будете продавать?")
+var questionUsers5 = prompt("Какой тип товара будете продавать?")
+
+var shopGoods = [questionUsers3, questionUsers4, questionUsers5];
+
+
+
+
+
+confirm(shopGoods);
